675acbc3ce1299cc2fde10ee01a7303e60164578
lahcen <lahcenwirghanec11@gmail.com>
2025-12-09 14:44:09 +0100

    Create CustomUserDetails and CustomUserDetailsService

---
+++ src/main/java/com/ouirghane/smartshop/security/CustomUserDetails.java
@@ -0,0 +1,66 @@
+package com.ouirghane.smartshop.security;
+
+import com.ouirghane.smartshop.entity.User;
+import com.ouirghane.smartshop.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private Long id;
+    private String username;
+    private String password;
+    private UserRole role;
+
+    public static CustomUserDetails fromUser(User user) {
+        return new CustomUserDetails(
+                user.getId(),
+                user.getUsername(),
+                user.getPassword(),
+                user.getRole()
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
