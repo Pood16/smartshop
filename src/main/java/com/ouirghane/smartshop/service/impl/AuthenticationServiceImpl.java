9019790492871e9475a70bac3dbf9bd738511d92
lahcen <lahcenwirghanec11@gmail.com>
2025-12-09 14:51:27 +0100

    Update authentication service to generate JWT tokens

--- src/main/java/com/ouirghane/smartshop/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/ouirghane/smartshop/service/impl/AuthenticationServiceImpl.java
@@ -5,9 +5,13 @@ import com.ouirghane.smartshop.dto.response.LoginResponseDto;
 import com.ouirghane.smartshop.entity.User;
 import com.ouirghane.smartshop.exception.AuthenticationException;
 import com.ouirghane.smartshop.repository.UserRepository;
+import com.ouirghane.smartshop.security.CustomUserDetails;
+import com.ouirghane.smartshop.security.JwtService;
 import com.ouirghane.smartshop.service.AuthenticationService;
-import com.ouirghane.smartshop.util.PasswordUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,19 +19,24 @@ import org.springframework.stereotype.Service;
 public class AuthenticationServiceImpl implements AuthenticationService {
 
     private final UserRepository userRepository;
-    private final PasswordUtil passwordUtil;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     @Override
     public LoginResponseDto login(LoginRequestDto request) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+        );
+
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        String token = jwtService.generateToken(userDetails);
+
         User user = userRepository.findByUsername(request.getUsername())
                 .orElseThrow(() -> new AuthenticationException("Invalid credentials"));
 
-        if (!passwordUtil.verifyPassword(request.getPassword(), user.getPassword())) {
-            throw new AuthenticationException("Invalid credentials");
-        }
-
         return LoginResponseDto
                 .builder()
+                .token(token)
                 .userId(user.getId())
                 .username(user.getUsername())
                 .role(user.getRole())
